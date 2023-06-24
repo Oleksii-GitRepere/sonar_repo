3e526aa5bbb5de65e2bfcc3056203dfa027cc995
Oleksii-GitRepere <119068230+Oleksii-GitRepere@users.noreply.github.com>
2023-06-24 17:52:27 +0300

    Update SimplecrudApplication.java

--- src/main/java/softserve/academy/simplecrud/SimplecrudApplication.java
+++ src/main/java/softserve/academy/simplecrud/SimplecrudApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /* Pull-request comment*/
 @SpringBootApplication
-/* public class SimplecrudApplication { */
-   public class SimplecrudApplicatio {
+public class SimplecrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimplecrudApplication.class, args);
